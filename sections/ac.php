144de9f12c5f70a6f0efba376a9cd7530513e3fc
okarachidera <45210306+okarachidera@users.noreply.github.com>
2020-11-17 14:40:03 +0100

    newupdate

---
+++ sections/ac.php
@@ -0,0 +1,105 @@
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/stylesection.css" >
+    </head>
+    <body>
+
+ 
+          <!-- Full Page Intro -->
+          <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/architecture.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-gradient align-items-center">
+              <!-- Content -->
+              <div class="container">
+                <!--Grid row-->
+                <div class="row">
+                  
+                  <!--Grid column-->
+                  <div class="col-md-4 white-text text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft" data-wow-delay="0.3s">
+                    <h1 class="h1-responsive font-weight-bold mt-sm-5"> Internet of Things (IoT) Based Smart Home Using Deep Learning Algorithm</h1>
+                    <hr class="hr-light">
+                    <h4 class="">By</h4>
+                    <h4 class="mb-4 font-weight-bold">OKARA, Chidera Chibuzor PG.2018/01273</h4> 
+                  </div>
+
+                  <!--Grid column-->
+                  <div class="col-md-4 col-xl-5 mt-xl-5 wow fadeInRight" data-wow-delay="0.3s">
+        
+                    <div class="smartphone" >
+                        <div class="content"style="background-image: url(../img/saver.jpg);">
+                        
+                            <div class="row" id="rowmbc">
+                                <div class="col-3">
+
+                                </div>
+
+                                <div class="col-6">
+                                    <br>
+                                    <br>
+
+                                    <label class="switch"><input type="checkbox" id="togBtn">
+                                        <div class="slider round">
+                                            <!--ADDED HTML -->
+                                            <span class="on">ON</span>
+                                            <span class="off">OFF</span>
+                                            <!--END-->
+                                        </div>
+                                    </label>
+                                </div>
+
+                                
+
+                                <div class="col-3">
+
+                                </div>
+                            </div>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+
+                            <div class="row">
+
+                                <div class="col-4">
+
+                                </div>
+
+                                <div class="col-4">
+                                    <a href="../index.php"><img src="../icons/home.png" alt="" height="80px"></a>
+                                </div>
+
+                                <div class="col-4">
+
+                                </div>
+                            </div>
+                        
+                            
+            
+                        </div>
+                  </div>
+                  <div class="col-md-4 col-xl-5 mt-xl-5 wow fadeInRight" data-wow-delay="0.3s">
+                    
+                  </div>
+                  <!--Grid column-->
+                </div>
+                <!--Grid row-->
+              </div>
+              <!-- Content -->
+            </div>
+            <!-- Mask & flexbox options-->
+          </div>
+          <!-- Full Page Intro -->
+        </header>
+        <!-- Main navigation -->
+
+
+    </body>
+</html>
---
+++ sections/light.php
@@ -0,0 +1,190 @@
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/stylesection.css" >
+    </head>
+    <body>
+
+ 
+          <!-- Full Page Intro -->
+          <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/architecture.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-gradient align-items-center">
+              <!-- Content -->
+              <div class="container">
+                <!--Grid row-->
+                <div class="row">
+                  
+                  <!--Grid column-->
+                  <div class="col-md-4 white-text text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft" data-wow-delay="0.3s">
+                    <h1 class="h1-responsive font-weight-bold mt-sm-5"> Internet of Things (IoT) Based Smart Home Using Deep Learning Algorithm</h1>
+                    <hr class="hr-light">
+                    <h4 class="">By</h4>
+                    <h4 class="mb-4 font-weight-bold">OKARA, Chidera Chibuzor PG.2018/01273</h4> 
+                  </div>
+
+                  <!--Grid column-->
+                  <div class="col-md-4 col-xl-5 mt-xl-5 wow fadeInRight" data-wow-delay="0.3s">
+        
+                    <div class="smartphone">
+                        <div class="content"style="background-image: url(../img/saver.jpg);">
+
+                        <div class="row">
+
+                              <div class="col-4">
+
+                              </div>
+
+                              <div class="col-4">
+                                  <p id="statusdate"></p>
+                                  <script>
+                                    var d = new Date();
+                                    var n = d.toLocaleTimeString();
+                                    document.getElementById("statusdate").innerHTML = n;
+                                  </script>
+                              </div>
+
+                              <div class="col-4">
+
+                              </div>
+                        </div>
+                        
+                            <div class="row" id="rowmbc">
+                                <div class="col-6">
+
+                          
+                                   
+                                      <h6 class="mb-4">Bedroom light</h6>
+                                    <label class="switch"><input type="checkbox" id="togBtn">
+                                        <div class="slider round">
+                                            <!--ADDED HTML -->
+                                            <span class="on">ON</span>
+                                            <span class="off">OFF</span>
+                                            <!--END-->
+                                        </div>
+                                    </label>
+
+                                </div>
+
+                                <div class="col-6">
+                                      
+                                    <h6 class="mb-4">Parlour light</h6>
+
+                                    <label class="switch"><input type="checkbox" id="togBtn">
+                                        <div class="slider round">
+                                            <!--ADDED HTML -->
+                                            <span class="on">ON</span>
+                                            <span class="off">OFF</span>
+                                            <!--END-->
+                                        </div>
+                                    </label>
+                                </div>
+
+                                
+
+                                <div class="col-6">
+                                  
+                                       
+                                    <h6 class="mb-4">Kitchen light</h6>
+
+                                    <label class="switch"><input type="checkbox" id="togBtn">
+                                        <div class="slider round">
+                                            <!--ADDED HTML -->
+                                            <span class="on">ON</span>
+                                            <span class="off">OFF</span>
+                                            <!--END-->
+                                        </div>
+                                    </label>
+
+                                </div>
+
+                                <div class="col-6">
+                                  
+                              
+                                     
+                                      <h6 class="mb-4">Toilet light</h6>
+  
+                                      <label class="switch"><input type="checkbox" id="togBtn">
+                                          <div class="slider round">
+                                              <!--ADDED HTML -->
+                                              <span class="on">ON</span>
+                                              <span class="off">OFF</span>
+                                              <!--END-->
+                                          </div>
+                                      </label>
+  
+                                </div>
+
+                                <div class="col-6">
+                                  
+                                 
+                                     
+                                      <h6 class="mb-4">Corridor light</h6>
+  
+                                      <label class="switch"><input type="checkbox" id="togBtn">
+                                          <div class="slider round">
+                                              <!--ADDED HTML -->
+                                              <span class="on">ON</span>
+                                              <span class="off">OFF</span>
+                                              <!--END-->
+                                          </div>
+                                      </label>
+  
+                                </div>
+
+                                <div class="col-6">
+                                  
+                                 
+                          
+                                      <h6 class="mb-4">Security light</h6>
+  
+                                      <label class="switch"><input type="checkbox" id="togBtn">
+                                          <div class="slider round">
+                                              <!--ADDED HTML -->
+                                              <span class="on">ON</span>
+                                              <span class="off">OFF</span>
+                                              <!--END-->
+                                          </div>
+                                      </label>
+  
+                                </div>
+                            </div>
+
+  
+                            <div class="row">
+
+                                <div class="col-4">
+
+                                </div>
+
+                                <div class="col-4">
+                                    <a href="../index.php"><img src="../icons/home.png" alt="" height="80px"></a>
+                                </div>
+
+                                <div class="col-4">
+
+                                </div>
+                            </div>
+                        
+                            
+            
+                        </div>
+                  </div>
+                  <div class="col-md-4 col-xl-5 mt-xl-5 wow fadeInRight" data-wow-delay="0.3s">
+                    
+                  </div>
+                  <!--Grid column-->
+                </div>
+                <!--Grid row-->
+              </div>
+              <!-- Content -->
+            </div>
+            <!-- Mask & flexbox options-->
+          </div>
+          <!-- Full Page Intro -->
+        </header>
+        <!-- Main navigation -->
+
+
+    </body>
+</html>
