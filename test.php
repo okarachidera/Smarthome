c714bbb3dda0508a135dce57f7711146cdbb9e84
okara chidera <45210306+okarachidera@users.noreply.github.com>
2021-09-09 11:23:08 +0100

    Delete test.php

--- test.php
+++
@@ -1,6 +0,0 @@
-<!-- <input type="checkbox" name="onoffswitch" id="yonoffswitch" -->
-<?php
-		$set='123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&';
-	echo	$code=substr(str_shuffle($set), 0, 6);
-
-?>
